d9d8f9a362190c39680f057ee34a245cc0911d12
Aleksandr Malakhov <schwarcroza@mail.ru>
2023-04-18 21:12:55 +0600

    create console programm for filter short strings

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string[] arr = ReadeConsole("Введите элементы массива через запятую: ").Split(',');
+string[] newArr = FilterShortStrings(arr, 3);
+PrintResult(newArr);
+
+///////////////////////////////////////////////////////////////////////////////////
+string[] FilterShortStrings(string[] arr, int length)
+{
+    string[] newArr = new string[arr.Length];
+    int index = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Trim().Length <= length)
+        {
+            newArr[index] = arr[i];
+            index++;
+        }
+    }
+    return newArr;
+}
+
+void PrintResult(string[] strs)
+{
+    int count = 0;
+    for(int i = 0; i < strs.Length; i++)
+    {        
+        if(!string.IsNullOrEmpty(strs[i]))
+        {
+            if(count > 0)
+            {
+                Console.Write(",");
+            }
+
+            Console.Write(strs[i]);
+            count++;
+        }
+    }
+}
+
+string ReadeConsole(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
